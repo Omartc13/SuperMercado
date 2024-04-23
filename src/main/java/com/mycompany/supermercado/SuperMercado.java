800c2b04045a89d0895479eeec82fbe1d2d2201f
Omar <118316814+Omartc13@users.noreply.github.com>
2024-04-23 13:54:57 -0500

    Add files via upload

---
+++ src/main/java/com/mycompany/supermercado/SuperMercado.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.supermercado;
+
+/**
+ *
+ * @author Omar Tc
+ */
+public class SuperMercado {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
